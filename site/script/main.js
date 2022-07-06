d07041c3328a2786eb606cabea776a16167d067d
Kremez Roman <romaratmir@gmail.com>
2022-07-06 19:00:30 +0300

    responsive menu

---
+++ site/script/main.js
@@ -0,0 +1,13 @@
+let burger = document.querySelector('.burger')
+let menu = document.querySelector('.menu')
+burger.addEventListener('click',function(e){
+    burger.classList.toggle('burger-active')
+    menu.classList.toggle('menu-active')
+    
+    // if(menu.classList.contains('menu-active')){
+    //     menu.classList.remove('menu-active')
+    // }else{
+    //     menu.classList.add('menu-active')
+    // }
+
+})
